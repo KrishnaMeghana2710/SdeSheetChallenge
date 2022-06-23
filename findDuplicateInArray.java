2e8434e6a82e12351b0102ce46bab24271a0a22f
KrishnaMeghana2710 <85273798+KrishnaMeghana2710@users.noreply.github.com>
2022-06-23 14:48:06 +0530

    Create findDuplicateInArray.java

---
+++ findDuplicateInArray.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        // Write your code here.
+       Map<Integer, Integer> mp= new HashMap<Integer, Integer>();
+        for(int i: arr)
+        {
+            if(mp.containsKey(i))
+            {
+                mp.put(i, mp.get(i)+1);
+            }
+            else
+                mp.put(i, 1);
+        }
+        for(int i: mp.keySet())
+        {
+            if(mp.get(i)!=1)
+                return i;
+         }
+        return 1;
+    }
+}
